8fee5076e899335247693f7d05319a9847a3d77e
rafamatic <rafayahmed3003@gmail.com>
2023-06-03 16:13:54 -0700

    products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,39 +5,41 @@ import Header from '../components/header'
 import HeroSection from '../components/hero'
 import TwoColumnComponent from '../components/section'
 import Carousel from '@/components/product_slider'
-const products = [
-  {
-    id: 1,
-    name: "Product 1",
-    image: "/card1.jpg",
-    description: "This is the first product.",
-  },
-  {
-    id: 1,
-    name: "Product 1",
-    image: "/card2.jpg",
-    description: "This is the first product.",
-  },
-  {
-    id: 1,
-    name: "Product 1",
-    image: "/card3.jpg",
-    description: "This is the first product.",
-  },
-  {
-    id: 1,
-    name: "Product 1",
-    image: "/card1.jpg",
-    description: "This is the first product.",
-  },
-  // Add more product objects here
-];
+import SingleProduct from '@/components/single_product'
+// const products = [
+//   {
+//     id: 1,
+//     name: "Product 1",
+//     image: "/card1.jpg",
+//     description: "This is the first product.",
+//   },
+//   {
+//     id: 1,
+//     name: "Product 1",
+//     image: "/card2.jpg",
+//     description: "This is the first product.",
+//   },
+//   {
+//     id: 1,
+//     name: "Product 1",
+//     image: "/card3.jpg",
+//     description: "This is the first product.",
+//   },
+//   {
+//     id: 1,
+//     name: "Product 1",
+//     image: "/card1.jpg",
+//     description: "This is the first product.",
+//   },
+//   // Add more product objects here
+// ];
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <CategoryCards />
-      <Carousel products={products} />
+      {/* <Carousel products={products} /> */}
+      <SingleProduct />
       <TwoColumnComponent />
     </div>
   )
