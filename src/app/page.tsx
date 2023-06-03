3d803e549da9f88ee1657ca4019d5992774e535e
rafamatic <rafayahmed3003@gmail.com>
2023-06-03 11:30:14 -0700

    hero responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,41 @@ import Footer from '../components/footer'
 import Header from '../components/header'
 import HeroSection from '../components/hero'
 import TwoColumnComponent from '../components/section'
-
+import Carousel from '@/components/product_slider'
+const products = [
+  {
+    id: 1,
+    name: "Product 1",
+    image: "/card1.jpg",
+    description: "This is the first product.",
+  },
+  {
+    id: 1,
+    name: "Product 1",
+    image: "/card2.jpg",
+    description: "This is the first product.",
+  },
+  {
+    id: 1,
+    name: "Product 1",
+    image: "/card3.jpg",
+    description: "This is the first product.",
+  },
+  {
+    id: 1,
+    name: "Product 1",
+    image: "/card1.jpg",
+    description: "This is the first product.",
+  },
+  // Add more product objects here
+];
 export default function Home() {
   return (
     <div>
-      <HeroSection/>
-      <CategoryCards/>
-      <TwoColumnComponent/>
+      <HeroSection />
+      <CategoryCards />
+      <Carousel products={products} />
+      <TwoColumnComponent />
     </div>
   )
 }
