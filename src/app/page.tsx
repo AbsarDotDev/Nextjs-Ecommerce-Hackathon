9a9fde5ef23296ab9177e327df5ddfcbe59ff22f
rafamatic <rafayahmed3003@gmail.com>
2023-06-04 13:43:52 -0700

    category on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,59 +5,85 @@ import Footer from '../components/footer'
 import Header from '../components/header'
 import HeroSection from '../components/hero'
 import TwoColumnComponent from '../components/section'
+import { Image as IImage } from 'sanity'
 import SingleProduct from '@/components/single_product'
 import ProductCard from '@/components/single_product'
-export default function Home() {
+import { client } from './lib/sanityClient'
+import CategoryCardsHome from '@/components/category_cards_home'
+
+export const getCat = async () => {
+  const res = await client.fetch(`*[_type == 'category'] | order(_random) [0...4]`);
+  return res;
+}
+
+export interface ICat {
+  _id: string,
+  name: string,
+  image: IImage
+}
+
+// export const getPro = async () => {
+//   const res = await client.fetch(`*[_type == 'category'] | order(_random) [0...4]`);
+//   return res;
+// }
+
+// export interface IPro {
+//   _id: string,
+//   name: string,
+//   image: IImage,
+// }
+
+export default async function Home() {
+  const data: ICat[] = await getCat();
   return (
     <div>
-      <Header/>
-      <HeroSection/>
+      <Header />
+      <HeroSection />
       {/* Category Section */}
 
       <section className="px-20 max-w-screen-6xl w-full">
 
-      <div>
-        <h3 className="font-bold text-2xl text-gray-800 leading-5">
-          Curated asset collections
-        </h3>
-        <h5 className="text-gray-800 font-light text-lg pt-2 mb-5">
-          Explore diverse collections of our most incredible high-resolution,
-          royalty-free, stock assets
-        </h5>
-      </div>
-      <div className="grid grid-cols-[auto] md:grid-cols-[auto,auto] custom:grid-cols-[auto,auto] lg:grid-cols-[auto,auto,auto,auto] justify-between">
-        <CategoryCards/>
-        <CategoryCards/>
-        <CategoryCards/>
-        <CategoryCards/>
- 
-      </div>
+        <div>
+          <h3 className="font-bold text-2xl text-gray-800 leading-5">
+            Curated asset collections
+          </h3>
+          <h5 className="text-gray-800 font-light text-lg pt-2 mb-5">
+            Explore diverse collections of our most incredible high-resolution,
+            royalty-free, stock assets
+          </h5>
+        </div>
+        <div className="grid grid-cols-[auto] md:grid-cols-[auto,auto] custom:grid-cols-[auto,auto] lg:grid-cols-[auto,auto,auto,auto] justify-between">
+          {data.map(
+            (item: any) => { return (<div key={item._id} > <CategoryCardsHome category={item} /></div>) }
+          )}
 
-
-</section>
-{/* Best Selling Section */}
-      <section className='best-selling mt-20'>
-      <div className='px-20 pt-20 flex flex-col items-start'>
-            <h3 className="font-bold text-2xl text-gray-800 leading-5  pb-5">
-                Best Selling
-            </h3>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-<ProductCard/>
-<ProductCard/>
-<ProductCard/>
-<ProductCard/>
-
-            </div>
-            <button className='bg-gradient-to-l from-primary-lightpink to-primary-pink py-3 px-8 text-white rounded-3xl mt-5'>
-                <Link href={'#'} className=''>
-                    View All
-                </Link>
-            </button>
         </div>
 
 
-        </section>
-      <TwoColumnComponent/>
+      </section>
+      {/* Best Selling Section */}
+      <section className='best-selling mt-20'>
+        <div className='px-20 pt-20 flex flex-col items-start'>
+          <h3 className="font-bold text-2xl text-gray-800 leading-5  pb-5">
+            Best Selling
+          </h3>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+
+          </div>
+          <button className='bg-gradient-to-l from-primary-lightpink to-primary-pink py-3 px-8 text-white rounded-3xl mt-5'>
+            <Link href={'#'} className=''>
+              View All
+            </Link>
+          </button>
+        </div>
+
+
+      </section>
+      <TwoColumnComponent />
       {/*@ts-ignore */}
       <Footer />
 
--- src/components/category_cards_home.tsx
+++ src/components/category_cards_home.tsx
@@ -2,28 +2,27 @@ import Image from "next/image";
 import HeroPoster from "/src/assets/images/Card1.png";
 import Link from "next/link";
 import { ICat } from "@/app/page";
+import { urlForImage } from "@/lib/image";
 
 interface CategoryCardsProps {
   category: ICat;
 }
-const CategoryCards = ({ category }: CategoryCardsProps) => {
+const CategoryCardsHome = ({ category }: CategoryCardsProps) => {
   return (
-    <div className="w-full max-w-sm">
+    <div className=" max-w-sm">
       <Link href={`/shop/${category.name}`}>
         <Image
           className="bg-white p-1 max-h-[300px] object-cover"
-          src={''} width={400} height={400}
+          src={urlForImage(category.image).url()} width={300} height={300}
           alt="Logo"
         ></Image>
-        <h6 className="font-bold text-[16px] pt-3 pb-1">
+
+        <h6 className="font-bold text-[32px] pt-3 pb-1 text-center">
           {category.name}
         </h6>
-        <p className="text-gray-800 text-[14px] pb-4">
-          {category.description}
-        </p>
       </Link>
     </div>
   )
 };
 
-export default CategoryCards;
+export default CategoryCardsHome;
